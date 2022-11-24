355a148e8aeea64498bb8f02436c64e13618ad2e
Mari <mstanyukova@gmail.com>
2022-11-24 21:10:26 +0600

    Updates

---
+++ precache-manifest.dcd43f6e3fc9962abe719e84614c4ebb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ad084de2fcbded1cbf1",
+    "url": "%7BStanyukova%7D.github.io/%7Btodo_react_firebase%7D/static/css/main.6d13d5ef.chunk.css"
+  },
+  {
+    "revision": "6ad084de2fcbded1cbf1",
+    "url": "%7BStanyukova%7D.github.io/%7Btodo_react_firebase%7D/static/js/main.b1e2c377.chunk.js"
+  },
+  {
+    "revision": "8dc9294f652c906546fb",
+    "url": "%7BStanyukova%7D.github.io/%7Btodo_react_firebase%7D/static/js/runtime~main.f1879074.js"
+  },
+  {
+    "revision": "04284401e8fdd7fe01b4",
+    "url": "%7BStanyukova%7D.github.io/%7Btodo_react_firebase%7D/static/js/2.77ed71ba.chunk.js"
+  },
+  {
+    "revision": "7621bc6736f774071bfc",
+    "url": "%7BStanyukova%7D.github.io/%7Btodo_react_firebase%7D/static/js/3.1d9e4d91.chunk.js"
+  },
+  {
+    "revision": "c6730ab949c2c8b23960a79fb7d23307",
+    "url": "%7BStanyukova%7D.github.io/%7Btodo_react_firebase%7D/index.html"
+  }
+];
