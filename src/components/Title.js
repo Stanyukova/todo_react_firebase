f48cf41127c13cd748f787a05601a0a81e56ce25
Mari <mstanyukova@gmail.com>
2022-11-24 19:23:05 +0600

    change title,css

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,9 +1,20 @@
 import React from "react";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete"; 
 
 export default function Title() {
   return (
     <div className="title">
       <h1>Todo App</h1>
+      <h2> Пояснительная бригада:</h2>
+      <div className="info"> Задача выполнена
+        <CheckCircleIcon  id="i2" /></div>
+     <div className="info"> Сохранить изменения
+     <EditIcon id="i2" /></div>
+      <div className="info"> Удалить задачу
+        <DeleteIcon  id="i2" /></div>
+     
     </div>
   );
 }
